9e83ba5e87ff75a50fd83c33ebeaca4a102f5c0d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-03 22:42:09 +0530

    app clean up

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -23,19 +23,28 @@ const Login = ({ classes }) => {
   
   // Onsuccesss of signin sending token to backend
   const onSuccess = async googleUser => {
-    const idToken = googleUser.getAuthResponse().id_token
-    const client = new GraphQLClient('http://localhost:4000/graphql',{
-      headers: { authorization: idToken }
-    })
-    const data = await client.request(ME_QUERY)
-    // console.log({ data })
-    dispatch({ type: "LOGIN_USER", payload: data.me })
+    try {
+      const idToken = googleUser.getAuthResponse().id_token
+      const client = new GraphQLClient('http://localhost:4000/graphql',{
+        headers: { authorization: idToken }
+      })
+      const data = await client.request(ME_QUERY)
+      dispatch({ type: "LOGIN_USER", payload: data.me })
+    } 
+    catch(err) {
+      onFailure(err)
+    }
+  };
+
+  const onFailure = err => {
+    console.error("Error Logging in", err)
   }
 
   return (
     <GoogleLogin 
       clientId="868408474848-q2f5ujaq4fh0qc4jg9tq1lrujvio7h77.apps.googleusercontent.com"
       onSuccess={onSuccess}
+      onFailure={onFailure}
       isSignedIn={true}
     />
   )
