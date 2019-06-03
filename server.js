b14f7537f661f8ae29745a6f6f3372fac220e11b
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-03 14:27:20 +0530

    adding test resolver and connecting to mongodb atlas

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-new ApolloServer({
-  typeDefs:"",
-  resolvers:""
-})
+//GraphQl Typedefs and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
+  .then(() => console.log("DB CONNECTED"))
+  .catch(err => console.log(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`)
+});
