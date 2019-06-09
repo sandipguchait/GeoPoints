cba73b691b729168b3b20ca665fbf763ae8fd0dd
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-09 15:48:17 +0530

    fixing styles

--- client/src/components/Pin/NoContent.js
+++ client/src/components/Pin/NoContent.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import ExploreIcon from "@material-ui/icons/Explore";
 import Typography from "@material-ui/core/Typography";
+import  MapIcon  from '@material-ui/icons/Map'
 
 const NoContent = ({ classes }) => (
   <div className={classes.root}>
@@ -16,18 +17,26 @@ const NoContent = ({ classes }) => (
     >
       Click on the map to add a pin
     </Typography>
+    <MapIcon className={classes.icon2}/>
   </div>
 )
 
 const styles = theme => ({
   root: {
     display: "flex",
+    backgroundColor: "smokeWhite",
     alignItems: "center",
     flexDirection: "column",
     justifyContent: "center"
   },
   icon: {
     margin: theme.spacing.unit,
+    color: "#746AFA",
+    fontSize: "80px"
+  },
+  icon2: {
+    margin: theme.spacing.unit,
+    color: "#66B266",
     fontSize: "80px"
   }
 });
