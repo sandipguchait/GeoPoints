0fc1b30291668c139c75916b55ef9fd5006dc28b
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-06-13 23:16:33 +0530

    updating files

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const { ApolloServer } = require('apollo-server');
+const express = require('express');
+const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -10,6 +13,12 @@ const { findOrCreateUser } = require('./controllers/userController');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true})
   .then(() => console.log("DB CONNECTED"))
   .catch(err => console.log(err));
+
+app.use(cors());
+
+app.use(express.static('public'));
+
+
 
 const server = new ApolloServer({
   typeDefs,
@@ -32,6 +41,20 @@ const server = new ApolloServer({
   }
 });
 
-server.listen().then(({ url }) => {
+server.applyMiddleware({
+  path: '/traveller', // you should change this to whatever you want
+  app,
+});
+
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
+  })
+}
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server listening on ${url}`)
 });
